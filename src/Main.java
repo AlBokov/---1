b46e359ed963ae06c1631b3dec982189cbcbfa4c
AlBokov <95411491+AlBokov@users.noreply.github.com>
2024-02-29 16:01:07 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 15_491;
-        int one_mile = 20;
-        int bonus = ticket_price / one_mile;
+        int ticketPrice = 15_491;
+        int oneMile = 20;
+        int bonus = ticketPrice / oneMile;
 
         System.out.println("Вам начислено: " + bonus);
 
